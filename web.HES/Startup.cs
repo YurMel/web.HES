89df9915f6fd08aee8df012f228e26a49f4a3701
Denys Zaliznyak <dzaliznyak@gmail.com>
2019-02-25 18:36:16 +0200

    Moved to MySQL

--- web.HES/Startup.cs
+++ web.HES/Startup.cs
@@ -36,7 +36,7 @@ namespace web.HES
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
