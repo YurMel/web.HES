97b2380967f50a620c42797c51a3e599022015dd
CORE <namespacedevbox@gmail.com>
2019-03-26 15:48:18 +0200

    Added Administrators in modal dialog

--- web.HES/Pages/Settings/Administrators/Index.cshtml.cs
+++ web.HES/Pages/Settings/Administrators/Index.cshtml.cs
@@ -19,6 +19,12 @@ namespace web.HES.Pages.Settings.Administrators
         private readonly ApplicationDbContext _context;
         public IList<ApplicationUser> ApplicationUser { get; set; }
 
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public ApplicationUser User { get; set; }
+
         [BindProperty]
         public InputModel Input { get; set; }
 
@@ -40,5 +46,84 @@ namespace web.HES.Pages.Settings.Administrators
         {
             ApplicationUser = await _context.Users.ToListAsync();
         }
+
+        #region Invite
+
+        public IActionResult OnGetInviteAdmin()
+        {
+            return Partial("_Invite", this);
+        }
+
+        public async Task<IActionResult> OnPostInviteAdminAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                // Create new user
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var password = Guid.NewGuid().ToString();
+                var result = await _userManager.CreateAsync(user, password);
+                await _userManager.AddToRoleAsync(user, Roles.AdminRole);
+
+                // Create "invite" link
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var email = Input.Email;
+                var callbackUrl = Url.Page(
+                   "/Account/Invite",
+                    pageHandler: null,
+                    values: new { area = "Identity", code, email },
+                    protocol: Request.Scheme);
+
+                await _emailSender.SendEmailAsync(
+                    Input.Email,
+                    "Invite to HES",
+                    $"Please enter your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                StatusMessage = "Email has been sent";
+                return RedirectToPage("./Index");
+            }
+            return RedirectToPage("./Index");
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async Task<IActionResult> OnGetDeleteAdminAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            return Partial("_Delete", this);
+        }
+
+        public async Task<IActionResult> OnPostDeleteAdminAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.Users.FindAsync(id);
+
+            if (User != null)
+            {
+                _context.Users.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            StatusMessage = "Removal was successful";
+            return RedirectToPage("./Index");
+        }
+
+        #endregion
     }
 }
