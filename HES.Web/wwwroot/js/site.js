2ae1a6bc4168056c54971584ffbf64881c4b8f85
CORE <namespacedevbox@gmail.com>
2019-04-16 10:10:15 +0300

    Added Sidebar toggle

--- HES.Web/wwwroot/js/site.js
+++ HES.Web/wwwroot/js/site.js
@@ -4,4 +4,31 @@
 $(document).ready(function () {
     $('.loading').removeClass('d-flex');
     $('.loading').addClass('d-none');
+
+    var toggled = localStorage.getItem('ToggledSidebar');
+    if (toggled === 'true') {
+        ToggleSidebarClass();
+    }
 });
+
+function ToggleSidebar() {
+    ToggleSidebarClass();
+
+    var toggled = localStorage.getItem('ToggledSidebar');
+    if (toggled === 'true') {
+        localStorage.setItem('ToggledSidebar', 'false');
+    }
+    else {
+        localStorage.setItem('ToggledSidebar', 'true');
+    }
+}
+
+function ToggleSidebarClass() {
+    $('.custom-sidebar').toggleClass('toggled');
+    $('.page_labels').toggleClass('toggled');
+    $('.sidebar-settings').toggleClass('toggled');
+    $('.icon-arrow').toggleClass('toggled');
+    $('.copyright').toggleClass('toggled');
+    $('.loading').toggleClass('toggled');
+    $('.icon-hideez').toggleClass('toggled');
+}
