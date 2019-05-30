95070ae589ee700d4dcc968dcc0a1c24689d7a7a
CORE <namespacedevbox@gmail.com>
2019-05-30 12:15:10 +0300

    Removed test ping button

--- HES.Web/Pages/Devices/Index.cshtml.cs
+++ HES.Web/Pages/Devices/Index.cshtml.cs
@@ -1,14 +1,11 @@
 ﻿using HES.Core.Entities;
 using HES.Core.Interfaces;
-using HES.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace HES.Web.Pages.Devices
@@ -69,36 +66,6 @@ namespace HES.Web.Pages.Devices
             }
 
             return RedirectToPage("./Index");
-        }
-
-        // TEMP
-        public async Task<IActionResult> OnPostPing(string id)
-        {
-            //id = "D0A89E6BCD8D";
-            id = "D9EB34A3F4C9";
-
-            try
-            {
-                if (AppHub.IsDeviceConnectedToHost(id))
-                {
-                    var device = await AppHub.EstablishRemoteConnection(id, 4);
-
-                    if (device != null)
-                    {
-                        var pingData = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04 };
-                        var respData = await device.Ping(pingData);
-
-                        Debug.Assert(pingData.SequenceEqual(respData.Result));
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message);
-                Debug.WriteLine(ex.Message);
-            }
-
-            return RedirectToPage("./Index");
-        }
+        }        
     }
 }
