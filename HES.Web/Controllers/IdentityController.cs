b5e3e941f08b8192f82f64d8a6776a96aec1ec7a
CORE <namespacedevbox@gmail.com>
2019-09-20 21:56:43 +0300

    Fix web api

--- HES.Web/Controllers/IdentityController.cs
+++ HES.Web/Controllers/IdentityController.cs
@@ -27,94 +27,129 @@ namespace HES.Web.Controllers
 
         [Route("[action]")]
         [HttpPost]
-        public async Task<IActionResult> AuthN(Data data)
+        public async Task<IActionResult> AuthN(AuthModel authModel)
         {
-            if (data == null)
+            if (authModel == null)
             {
+                _logger.LogWarning("[API] Credentials is null");
                 return BadRequest(new { error = "CredentialsNullException" });
             }
 
             // Find user
-            var user = await _userManager.FindByEmailAsync(data.Email);
+            var user = await _userManager.FindByEmailAsync(authModel.Email);
             if (user == null)
             {
+                _logger.LogWarning($"[API] User {authModel.Email} not found");
                 return Unauthorized(new { error = "UserNotFoundException" });
             }
 
-            // Two factor requires
-            if (user.TwoFactorEnabled && data.Otp == null)
+            //// Verify password
+            //var passwordResult = await _signInManager.PasswordSignInAsync(data.Email, data.Password, false, lockoutOnFailure: true);
+            //if (passwordResult.Succeeded)
+            //{
+            //    _logger.LogInformation($"User {data.Email} succeeded auth via API");
+
+            //    await _signInManager.SignOutAsync();
+
+            //    return Ok(new User()
+            //    {
+            //        FirstName = user.FirstName,
+            //        LastName = user.LastName,
+            //        Email = user.Email,
+            //        PhoneNumber = user.PhoneNumber
+            //    });
+            //}
+
+            //// Verify two factor
+            //if (passwordResult.RequiresTwoFactor)
+            //{
+            //    if (string.IsNullOrWhiteSpace(data.Otp))
+            //    {
+            //        return Unauthorized(new { error = "TwoFactorRequiredException" });
+            //    }
+
+            //    var authenticatorCode = data.Otp.Replace(" ", string.Empty).Replace("-", string.Empty);
+
+            //    var twoFactorResult = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, false, false);
+
+            //    if (twoFactorResult.Succeeded)
+            //    {
+            //        _logger.LogInformation($"User {data.Email} succeeded auth via API with 2FA");
+
+            //        await _signInManager.SignOutAsync();
+
+            //        return Ok(new User()
+            //        {
+            //            FirstName = user.FirstName,
+            //            LastName = user.LastName,
+            //            Email = user.Email,
+            //            PhoneNumber = user.PhoneNumber
+            //        });
+            //    }
+            //    else if (twoFactorResult.IsLockedOut)
+            //    {
+            //        _logger.LogWarning($"User {user.Email} account locked out.");
+            //        return Unauthorized(new { error = "UserIsLockedoutException" });
+            //    }
+            //    else
+            //    {
+            //        _logger.LogWarning($"Invalid authenticator code entered for user {user.Email}.");
+            //        return Unauthorized(new { error = "InvalidAuthenticatorCodeException" });
+            //    }
+            //}
+
+            //// Is locked out
+            //if (passwordResult.IsLockedOut)
+            //{
+            //    _logger.LogWarning($"User account {user.Email} locked out.");
+            //    return Unauthorized(new { error = "UserIsLockedoutException" });
+            //}
+            //else
+            //{
+            //    _logger.LogError($"User {user.Email} unauthorized.");
+            //    return Unauthorized(new { error = "UnauthorizedException" });
+            //}
+
+
+            // Verify password
+            var passwordResult = await _userManager.CheckPasswordAsync(user, authModel.Password);
+            if (!passwordResult)
             {
-                return Unauthorized(new { error = "UserRequiresTwoFactor" });
-            }
-
-            // Clear the existing cookie to ensure a clean login process
-            //await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
-
-            // Sing In
-            var result = await _signInManager.PasswordSignInAsync(data.Email, data.Password, false, lockoutOnFailure: true);
-            if (result.Succeeded)
-            {
-                _logger.LogInformation($"User {data.Email} succeeded auth via API");
-
-                var response = new User()
-                {
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Email = user.Email,
-                    PhoneNumber = user.PhoneNumber
-                };
-
-                return Ok(response);
-            }
-            if (result.RequiresTwoFactor)
-            {
-                // Clear the existing cookie to ensure a clean login process
-                //await HttpContext.SignOutAsync(IdentityConstants.TwoFactorRememberMeScheme);
-
-                var authenticatorCode = data.Otp.Replace(" ", string.Empty).Replace("-", string.Empty);
-
-                var twoFactorResult = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, false, false);
-
-                if (twoFactorResult.Succeeded)
-                {
-                    _logger.LogInformation($"User {data.Email} succeeded auth via API with 2FA");
-
-                    var response = new User()
-                    {
-                        FirstName = user.FirstName,
-                        LastName = user.LastName,
-                        Email = user.Email,
-                        PhoneNumber = user.PhoneNumber
-                    };
-
-                    return Ok(response);
-                }
-                else if (twoFactorResult.IsLockedOut)
-                {
-                    _logger.LogWarning($"User {user.Email} account locked out.");
-                    return Unauthorized(new { error = "UserIsLockedoutException" });
-                }
-                else
-                {
-                    _logger.LogWarning($"Invalid authenticator code entered for user with ID {user.Id}, email {user.Email}.");
-                    return Unauthorized(new { error = "InvalidAuthenticatorCodeException" });
-                }
-
-            }
-            if (result.IsLockedOut)
-            {
-                _logger.LogWarning($"User {user.Email} account locked out.");
-                return Unauthorized(new { error = "UserIsLockedoutException" });
-            }
-            else
-            {
-                _logger.LogError($"User {user.Email} unauthorized.");
+                _logger.LogWarning($"[API] User {user.Email} verify password failed.");
                 return Unauthorized(new { error = "UnauthorizedException" });
             }
+
+            // Verify two factor
+            if (user.TwoFactorEnabled)
+            {
+                if (string.IsNullOrWhiteSpace(authModel.Otp))
+                {
+                    return Unauthorized(new { error = "TwoFactorRequiredException" });
+                }
+
+                var authenticatorCode = authModel.Otp.Replace(" ", string.Empty).Replace("-", string.Empty);
+
+                var tokenResult = await _userManager.VerifyTwoFactorTokenAsync(user, _userManager.Options.Tokens.AuthenticatorTokenProvider, authenticatorCode);
+                if (!tokenResult)
+                {
+                    _logger.LogWarning($"[API] User {authModel.Email} verify 2fa failed.");
+                    return Unauthorized(new { error = "InvalidAuthenticatorCodeException" });
+                }
+            }
+
+            _logger.LogInformation($"[API] User {authModel.Email} verified successed");
+
+            return Ok(new User()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            });
         }
     }
 
-    public class Data
+    public class AuthModel
     {
         public string Email { get; set; }
         public string Password { get; set; }
